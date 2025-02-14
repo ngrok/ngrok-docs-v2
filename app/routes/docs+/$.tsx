4274baded5aa12b15a3542c379ce164100277a4b
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-14 10:57:35 -0500

    Add content from main in our prod docs

--- app/routes/docs+/$.tsx
+++ app/routes/docs+/$.tsx
@@ -1,12 +1,12 @@
 import type {
-	LoaderArgs,
-	V2_MetaFunction,
-	HeadersFunction,
-	SerializeFrom,
+  LoaderArgs,
+  V2_MetaFunction,
+  HeadersFunction,
+  SerializeFrom,
 } from "@vercel/remix";
-import { json, redirect } from '@vercel/remix';
-import { useLoaderData } from '@remix-run/react'
-import { parseISO, format } from 'date-fns';
+import { json, redirect } from "@vercel/remix";
+import { useLoaderData } from "@remix-run/react";
+import { parseISO, format } from "date-fns";
 
 import invariant from "tiny-invariant";
 
@@ -17,78 +17,99 @@ import { getContent } from "~/utils/blog.server";
 import { CacheControl } from "~/utils/cache-control.server";
 import { getSeo } from "~/seo";
 
-export const loader = async ({params}: LoaderArgs) => {
-	let path = params["*"];
+export const loader = async ({ params }: LoaderArgs) => {
+  let path = params["*"];
 
-	invariant(path, "BlogPost: path is required");
+  invariant(path, "BlogPost: path is required");
 
-	if (!path) {
-		throw new Error('path is not defined')
-	}    
+  if (!path) {
+    throw new Error("path is not defined");
+  }
 
-	const files = await getContent(`docs/${path}`);
-	let post = files && parseMarkdown(files[0].content);
+  const files = await getContent(`docs/${path}`);
+  let post = files && parseMarkdown(files[0].content);
 
-	//invariant(post, "Not found");
-	if (!post) {
-		throw json({}, {
-			status: 404, headers:  {}
-		})
-	}    
+  //invariant(post, "Not found");
+  if (!post) {
+    throw json(
+      {},
+      {
+        status: 404,
+        headers: {},
+      }
+    );
+  }
 
-	return json({post}, {
-		headers: { 
-			"Cache-Control": new CacheControl("swr").toString() 
-		}
-	})
-}
+  return json(
+    { post },
+    {
+      headers: {
+        "Cache-Control": new CacheControl("swr").toString(),
+      },
+    }
+  );
+};
 
-export const headers: HeadersFunction = ({loaderHeaders}) => {
-	return {
-		'Cache-Control': loaderHeaders.get('Cache-Control')!
-	}
-}
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  return {
+    "Cache-Control": loaderHeaders.get("Cache-Control")!,
+  };
+};
 
 export const meta: V2_MetaFunction = ({ data, matches }) => {
-	if(!data) return [];
+  if (!data) return [];
 
-	const parentData = matches.flatMap((match) => match.data ?? [] );
+  const parentData = matches.flatMap((match) => match.data ?? []);
 
-	return [
-		getSeo({
-			title: data.post.frontmatter.meta.title,
-			description: data.post.frontmatter.meta.description,
-			url: `${parentData[0].requestInfo.url}`,
-		}),  
-	]
-}
+  const {
+    post: { frontmatter },
+  } = data;
+  console.error("Post info", data.post);
+
+  return [
+    getSeo({
+      title: frontmatter?.meta?.title ?? frontmatter?.title,
+      description: frontmatter?.meta?.description ?? frontmatter?.description,
+      url: `${parentData[0].requestInfo.url}`,
+    }),
+  ];
+};
 
 export default function BlogPost() {
-	const {post} = useLoaderData<typeof loader>();
+  const { post } = useLoaderData<typeof loader>();
 
-	return (
-        <div className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
-			<article className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
-				<h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">{post.frontmatter.meta.title}</h1>
-				<div className="flex flex-col items-start justify-between w-full mt-2 md:flex-row md:items-center">
-					<div className="flex items-center space-x-2">
-						{post.frontmatter.date && <p className="text-sm text-gray-700 dark:text-gray-300">
-							{'Created: '}
-							{post.frontmatter.date && format(parseISO(post.frontmatter.date), 'MMMM dd, yyyy')}
-						</p>}
-						{post.frontmatter.updated && <p className="text-sm text-gray-700 dark:text-gray-300">
-							{'Last updated: '}
-							{post.frontmatter.updated && format(parseISO(post.frontmatter.updated), 'MMMM dd, yyyy')}
-						</p>}
-					</div>
-					<p className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
-						{post.readTime.text}
-					</p>
-				</div>
-				<div className="w-full mt-4 prose dark:prose-dark max-w-none">
-					{post.body && <MarkdownView content={post.body} />}
-				</div>
-			</article>
+  const { frontmatter, body } = post;
+  return (
+    <div className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
+      <article className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+          {frontmatter?.meta?.title ?? frontmatter?.title}
+        </h1>
+        <div className="flex flex-col items-start justify-between w-full mt-2 md:flex-row md:items-center">
+          <div className="flex items-center space-x-2">
+            {frontmatter?.date && (
+              <p className="text-sm text-gray-700 dark:text-gray-300">
+                {"Created: "}
+                {frontmatter?.date &&
+                  format(parseISO(frontmatter?.date), "MMMM dd, yyyy")}
+              </p>
+            )}
+            {frontmatter?.updated && (
+              <p className="text-sm text-gray-700 dark:text-gray-300">
+                {"Last updated: "}
+                {frontmatter?.updated &&
+                  format(parseISO(frontmatter?.updated), "MMMM dd, yyyy")}
+              </p>
+            )}
+          </div>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
+            {frontmatter?.readTime && frontmatter.readTime.text}
+          </p>
         </div>
-	)
+        <div className="w-full mt-4 prose dark:prose-dark max-w-none">
+          {body && <MarkdownView content={body} />}
+        </div>
+      </article>
+    </div>
+  );
 }
