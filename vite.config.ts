7d498477030f9a0dcb762737a275d5e67a9d7efe
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-20 13:45:31 -0500

    Rendering and routes is fixed; still need to address some thing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vitePluginRequire from "vite-plugin-require";
 import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-
+import { flatRoutes } from "remix-flat-routes";
+import path from "path";
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -13,11 +14,28 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+      "@components": path.resolve(__dirname, "./app/components/"),
+    },
+  },
   plugins: [
     mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
     remix({
+      routes(defineRoutes) {
+        return flatRoutes("routes", defineRoutes, {
+          ignoredRouteFiles: ["**/.*"], // Ignore dot files (like .DS_Store)
+          //appDir: 'app',
+          //routeDir: 'routes',
+          //basePath: '/',
+          //paramPrefixChar: '$',
+          //nestedDirectoryChar: '+',
+          //routeRegex: /((\${nestedDirectoryChar}[\/\\][^\/\\:?*]+)|[\/\\]((index|route|layout|page)|(_[^\/\\:?*]+)|([^\/\\:?*]+\.route)))\.(ts|tsx|js|jsx|md|mdx)$$/,
+        });
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
