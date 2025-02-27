a04252025af842604e3363b797e267058fdbe662
Shaquil Hansford <liuqahs15@gmail.com>
2025-02-27 11:26:44 -0500

    Checkpoint; fixing integrationlist component

--- app/components/IntegrationPageList.tsx
+++ app/components/IntegrationPageList.tsx
@@ -1,29 +1,33 @@
+/**
+ * Rework this component to just read whatever's in the directory and list that in the NgrokCard components, that way we don't need to have a bunch of state issues or any weirdness
+ */
+
 import { useIntegration } from "./integrations/use-integrations";
 import NgrokCard from "./NgrokCard";
 
 type Props = {
-	name: string;
+  name: string;
 };
 
 export default function IntegrationPageList({ name }: Props) {
-	const integration = useIntegration(name);
+  const integration = useIntegration(name);
 
-	if (!integration) {
-		return null;
-	}
+  if (!integration) {
+    return null;
+  }
 
-	return (
-		<ul className="m-0 mb-5 grid list-none grid-cols-2 gap-5 p-0" role="list">
-			{integration.docs.map((doc) => (
-				<li className="last-of-type:col-span-full" key={doc.path}>
-					<NgrokCard
-						to={doc.path}
-						size="sm"
-						title={doc.frontMatter?.title || doc.contentTitle}
-						description={doc.frontMatter?.description || doc.excerpt}
-					/>
-				</li>
-			))}
-		</ul>
-	);
+  return (
+    <ul className="m-0 mb-5 grid list-none grid-cols-2 gap-5 p-0" role="list">
+      {integration.docs.map((doc) => (
+        <li className="last-of-type:col-span-full" key={doc.path}>
+          <NgrokCard
+            to={doc.path}
+            size="sm"
+            title={doc.frontMatter?.title || doc.contentTitle}
+            description={doc.frontMatter?.description || doc.excerpt}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
--- app/components/IntegrationsList.tsx
+++ app/components/IntegrationsList.tsx
@@ -1,22 +1,20 @@
-import { useIntegrations } from "./integrations/use-integrations";
+import type { Integration } from "~/utils/Integrations/getIntegrations";
 import NgrokCard from "./NgrokCard";
 
-export default function IntegrationsList() {
-	const integrations = useIntegrations();
-
-	return (
-		<ul className="m-0 mb-5 grid list-none grid-cols-2 gap-5 p-0" role="list">
-			{integrations.map((integration) => (
-				<li className="last-of-type:col-span-full" key={integration.name}>
-					<NgrokCard
-						to={integration.path}
-						size="sm"
-						img={integration.metadata?.logo}
-						title={integration.metadata?.sidebar_label || integration.name}
-						description={integration.metadata?.excerpt}
-					/>
-				</li>
-			))}
-		</ul>
-	);
+export default function IntegrationsList({ list }: { list: Integration[] }) {
+  return (
+    <ul className="m-0 mb-5 grid list-none grid-cols-2 gap-5 p-0" role="list">
+      {list.map((integration) => (
+        <li className="last-of-type:col-span-full" key={integration.name}>
+          <NgrokCard
+            to={integration.path}
+            size="sm"
+            img={integration.metadata?.logo}
+            title={integration.metadata?.sidebar_label || integration.name}
+            description={integration.metadata?.excerpt}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
