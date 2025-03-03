868c7f9a9bcd171d8b48d048f6478b0908fa18b4
Shaquil Hansford <liuqahs15@gmail.com>
2025-03-03 13:10:44 -0500

    Starting to build out custom search component

--- app/components/Search/SearchHits/HitItem.tsx
+++ app/components/Search/SearchHits/HitItem.tsx
@@ -1,12 +1,45 @@
 import { Link } from "@remix-run/react";
+import { Hit } from "algoliasearch";
 
-export function HitItem({ hit }: { hit: any }) {
-  if (!hit?.hierarchy?.lvl1) return <p>Nothing</p>;
+export type DocHitItem = {
+  url: string;
+  url_without_anchor: string;
+  anchor: string;
+  content: any;
+  type: string;
+  hierarchy: {
+    lvl0: string; // Name of sidenav category it's under
+    lvl1: string; // h1 of the page
+    lvl2: string; // h2
+    lvl3: string; // etc ...
+    lvl4: string;
+    lvl5: string;
+    lvl6: string;
+  };
+  objectID: string;
+  _highlightResult: {
+    hierarchy: {
+      lvl0: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+      lvl1: {
+        value: string;
+        matchLevel: string;
+        matchedWords: string[];
+      };
+    };
+  };
+  __position: number;
+  __queryID: string;
+};
+
+export function HitItem({ hit }: { hit: Hit<DocHitItem> }) {
+  if (hit.anchor) console.log("Hit", JSON.stringify(hit, null, 2));
   return (
-    <article className="flex flex-col">
-      <Link to={hit.url}>
-        <p>{hit.hierarchy.lvl1}</p>
-      </Link>
-    </article>
+    <Link to={hit.url}>
+      <p>{hit.hierarchy.lvl1}</p>
+    </Link>
   );
 }
--- app/components/Search/SearchHits/index.tsx
+++ app/components/Search/SearchHits/index.tsx
@@ -1,6 +1,25 @@
 import { Hits } from "react-instantsearch";
 import { HitItem } from "./HitItem";
+import { useHits } from "react-instantsearch";
+import { Hit } from "algoliasearch";
 
-export function SearchHits() {
-  return <Hits hitComponent={HitItem} />;
+/**
+ * Map the hits such that hits within a page appear together in a sublist
+ * under the hit for the page itself, like in prod.
+ * If hit content exists, show a portion of it like in prod.
+ * Here, we can get really detailed with how we show search results
+ * since we own the search component. But that can be done in the future.
+ * For now just make it all work like prod.
+ */
+
+export function SearchHits(props: any) {
+  const { items, results, banner, sendEvent } = useHits(props);
+  return (
+    <div>
+      {items.map((item: Hit<any>) => (
+        <HitItem key={item.objectID} hit={item} />
+      ))}
+    </div>
+  );
+  // return <Hits hitComponent={HitItem} />;
 }
