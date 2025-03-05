f95e36cdd7a6a4126dd993268a40e8187e719431
Shaquil Hansford <liuqahs15@gmail.com>
2025-03-05 09:16:57 -0500

    Basic nav with accordion

--- app/components/layout/SidebarNav.tsx
+++ app/components/layout/SidebarNav.tsx
@@ -8,32 +8,71 @@ import {
 } from "@ngrok/mantle/accordion";
 import { Card, CardBody } from "@ngrok/mantle/card";
 import { Link } from "@remix-run/react";
+import type { SidebarItem } from "~/utils/sidebar";
 import sidebar from "~/utils/sidebar";
+
+/**
+ * TODO:
+ * - Check if you can pull the metadata from files
+ *  using their path. Meaning, tell remark to parse the file
+ *  at the provided path, and get the frontmatter details.
+ *  IF so, we can replicate current docusaurus behavior with just providing
+ *  the path. We can also replicate its ability to auto-generate a nav section
+ *  from a directory
+ */
+
+/**
+ * TODO:
+ * - Make the accordion containing the page you're on open automatically
+ * - Scrollto the item representing the page you're on
+ * - Make the page you're on highlighted
+ */
+
+export const SidebarSection = ({
+  sectionItem,
+}: {
+  sectionItem: SidebarItem;
+}) => {
+  console.log("Section item", sectionItem);
+  return (
+    <Accordion type="multiple">
+      <AccordionItem value={sectionItem.title}>
+        <AccordionHeading asChild>
+          <AccordionTrigger>
+            {sectionItem.title}
+            <AccordionTriggerIcon />
+          </AccordionTrigger>
+        </AccordionHeading>
+        <AccordionContent className="ml-5 flex flex-col">
+          {sectionItem.children.map((child: any) => {
+            return child.children ? (
+              <SidebarSection
+                key={child.path || child.title}
+                sectionItem={child}
+              />
+            ) : (
+              <Link to={child.path}>{child.title}</Link>
+            );
+          })}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
 
 export function SidebarNav({ className }: any) {
   return (
     <nav className={className}>
       <ul role="list">
         {sidebar &&
-          sidebar.map((section) => {
+          sidebar.map((topLevelItem: SidebarItem) => {
             return (
-              <li key={section.title}>
-                <Accordion type="multiple">
-                  <AccordionItem value={section.title}>
-                    <AccordionHeading asChild>
-                      <AccordionTrigger>
-                        {section.title}
-                        <AccordionTriggerIcon />
-                      </AccordionTrigger>
-                    </AccordionHeading>
-                    <AccordionContent className="ml-5 flex flex-col">
-                      {section.links &&
-                        section.links.map((link: any) => (
-                          <Link to={link.href}>{link.title}</Link>
-                        ))}
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
+              <li key={topLevelItem.path || topLevelItem.title}>
+                {topLevelItem.children ? (
+                  <SidebarSection sectionItem={topLevelItem} />
+                ) : (
+                  <Link to={topLevelItem.path}>{topLevelItem.title}</Link>
+                )}
               </li>
             );
           })}
--- app/utils/sidebar.ts
+++ app/utils/sidebar.ts
@@ -1,16 +1,29 @@
+export type SidebarItem = {
+  title: string;
+  path?: string;
+  children: SidebarItem[];
+};
+
 export default [
   {
     title: "Introduction",
-    links: [
-      { title: "Example", href: "/docs/example" },
-      { title: "Home", href: "/docs/" },
+    children: [
+      {
+        title: "Example",
+        path: "/docs/whats-new",
+        children: [
+          { title: "Example 3", path: "/docs/whats-new" },
+          { title: "Home 3", path: "/docs/overview/" },
+        ],
+      },
+      { title: "Home", path: "/docs/overview/" },
     ],
   },
   {
-    title: "Core Concepts",
-    links: [
-      { title: "Roadmap", href: "/docs/roadmap" },
-      { title: "Changelog", href: "/docs/changelog" },
+    title: "Section 2",
+    children: [
+      { title: "Example 2", path: "/docs/whats-new" },
+      { title: "Home 2", path: "/docs/overview/" },
     ],
   },
-];
+] as SidebarItem[];
