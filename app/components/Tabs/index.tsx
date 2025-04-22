7da8bd31b86ff2fbf2de7ec1755c622c9d690e8c
Shaquil Hansford <liuqahs15@gmail.com>
2025-04-22 12:17:34 -0400

    Finish Tabs component

--- app/components/Tabs/index.tsx
+++ app/components/Tabs/index.tsx
@@ -1,14 +1,15 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ngrok/mantle/tabs";
-import React from "react";
+import React, { useContext } from "react";
+import TabListContext from "./TabListContext";
 
-export default function TabsComponent(props: any) {
-  console.log("Tabs", props);
-  return (
-    <div className="tabs">
-      <div className="tabs__list">{props.children}</div>
-    </div>
-  );
-}
+// export default function TabsComponent(props: any) {
+//   console.log("Tabs", props);
+//   return (
+//     <div className="tabs">
+//       <div className="tabs__list">{props.children}</div>
+//     </div>
+//   );
+// }
 
 type TabsProps = {
   groupId?: string;
@@ -26,13 +27,50 @@ type TabsProps = {
 
 // Just map the tab conent twice
 
-export function NewTabsComponent(props: TabsProps) {
-  const defaultTab = getValidDefaultTab(tabValues, localStorageTab);
-  const tabToShow = getValidTabToShow(tabValues, selectedValue, defaultTab);
+type Tab = {
+  props: {
+    label?: string;
+    value?: string;
+    children: React.ReactNode[] | React.ReactNode;
+  };
+};
+
+export default function TabsComponent(props: TabsProps) {
+  const tabRefs: (HTMLButtonElement | null)[] = [];
+  const { localStorageTab, selectedTabItem, updateSelectedTabItem } =
+    useContext(TabListContext);
+  // Alphabetize the tabs
+  const tabs: any[] = props.children
+    .slice(0)
+    .sort((a: any, b: any) =>
+      a.props.label && b.props.label
+        ? a.props.label.localeCompare(b.props.label)
+        : -1
+    );
+
+  const handleTabChange = (
+    event:
+      | React.FocusEvent<HTMLButtonElement>
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLButtonElement>
+  ) => {
+    const newTab = event.currentTarget;
+    const newTabIndex = tabRefs.indexOf(newTab);
+    const newTabValue =
+      tabs[newTabIndex]?.props?.label || tabs[newTabIndex]?.props?.value;
+
+    if (newTabValue !== selectedTabItem && updateSelectedTabItem) {
+      updateSelectedTabItem(newTabValue);
+    }
+  };
+
+  const defaultTab = getValidDefaultTab(tabs, localStorageTab);
+  const tabToShow = getValidTabToShow(tabs, selectedTabItem, defaultTab);
+
   return (
-    <Tabs orientation="horizontal" defaultValue="account" className="w-[400px]">
+    <Tabs orientation="horizontal" defaultValue={defaultTab} value={tabToShow}>
       <TabsList>
-        {props.children?.map((tabItem: any, i: number) => {
+        {tabs?.map((tabItem: any, i: number) => {
           const { label, value } = tabItem.props;
           if (!label && !value) {
             throw new Error("TabItem must have a label or value");
@@ -41,15 +79,19 @@ export function NewTabsComponent(props: TabsProps) {
             <TabsTrigger
               value={label || value}
               role="tab"
-              // aria-selected={selectedTabItem === label}
+              aria-selected={selectedTabItem === label}
               key={`${value}${i}`}
+              onClick={handleTabChange}
+              ref={(tabControl) => {
+                tabRefs.push(tabControl);
+              }}
             >
               {label || value}
             </TabsTrigger>
           );
         })}
       </TabsList>
-      {props.children?.map((tabItem: any, i: number) => {
+      {tabs?.map((tabItem: any, i: number) => {
         const { label, value, children } = tabItem.props;
         return (
           <TabsContent value={label || value} key={`${value}${i}`}>
@@ -62,28 +104,32 @@ export function NewTabsComponent(props: TabsProps) {
 }
 
 function getValidDefaultTab(
-  tabs: readonly any[],
+  tabs: any[],
   localStorageTab: string | null | undefined
 ) {
-  const defaultTab = tabs.find(
-    (tab) =>
+  console.log("Localstorage tab", localStorageTab);
+  const defaultTab = tabs.find((tab) => {
+    console.log("Tab", tab.props.label);
+    return (
       tab.props.label === localStorageTab || tab.props.value === localStorageTab
-  );
+    );
+  });
   if (defaultTab) {
-    return defaultTab.label;
+    console.log("Returning default tab", defaultTab.props.label);
+    return defaultTab.props.label;
   }
-  return tabs[0]?.label;
+  return tabs[0]?.props.label;
 }
 
 function getValidTabToShow(
   tabs: any[],
-  selectedValue: string | undefined,
+  selectedValue: string | null,
   defaultTab: string | undefined
 ) {
   if (selectedValue) {
     const selectedTab = tabs.find((tab) => tab.props.label === selectedValue);
     if (selectedTab) {
-      return selectedTab.label;
+      return selectedTab.props.label;
     }
   }
   return defaultTab;
