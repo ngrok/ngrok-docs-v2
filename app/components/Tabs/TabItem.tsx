032e3b10a8f8eb648feb016dfc8cee7604813bf4
Shaquil Hansford <liuqahs15@gmail.com>
2025-09-09 16:01:51 -0400

    Remove everything

--- app/components/Tabs/TabItem.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * This component is a placeholder for the TabItem component.
- * See ./index.tsx for why we've structured it this way.
- */
-export default function TabItem(props: any) {
-  return <div {...props}>{props.children}</div>;
-}
--- app/utils/redirects/redirectAggregator.ts
+++
@@ -1,7 +0,0 @@
-import { generalRedirects } from "./data/general";
-/**
- * This file imports all redirects from ./data
- * and exports them as a single array.
- * When you make a new redirect file, import it here.
- */
-export const allRedirects = [...generalRedirects];
