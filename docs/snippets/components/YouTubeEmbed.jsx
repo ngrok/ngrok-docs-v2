02a7e8ea7537c65e5bdfaeb81c6f02bce201ca06
Shaquil Hansford <liuqahs15@gmail.com>
2025-09-10 15:25:04 -0400

    Set up some custom components

--- docs/snippets/components/YouTubeEmbed.jsx
+++ docs/snippets/components/YouTubeEmbed.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 
-export function YouTubeEmbed({
-  className,
-  title,
-  videoId,
-  ...props
-}: {
-  /**
-   * The ID of the YouTube video to embed.
-   * @example "dQw4w9WgXcQ" from https://www.youtube.com/watch?v=dQw4w9WgXcQ
-   */
-  videoId: string;
-  /**
-   * Accessible video title
-   * @example "Rick Astley - Never Gonna Give You Up (Video)"
-   */
-  title: string;
-  className?: string;
-}) {
+export const YouTubeEmbed = ({ className, title, videoId, ...props }) => {
   return (
     <div className={`relative aspect-video mb-3 ${className}`} {...props}>
       <iframe
@@ -29,4 +12,4 @@ export function YouTubeEmbed({
       />
     </div>
   );
-}
+};
